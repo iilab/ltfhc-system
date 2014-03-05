1cf5a4e1f007072bcafc9cd544736722dcb876c2
Aaron Huslage <huslage@gmail.com>
2014-03-05 16:31:27 -0500

    added couch configs. updated ppp options

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,14 +16,18 @@ function the_trap_handler()
 #
 trap 'the_trap_handler ${LINENO} $?' ERR
 
+SD=${BASH_SOURCE%/*}
+
 # Automatically re-run under sudo if we aren't root.
 if [ $(id -u) != 0 ]; then
    echo "This script requires root permissions"
-   sudo "$0"
+   sudo ${BASH_SOURCE}
    exit
 fi
 
-echo "*** Deployment started at $(date)" >> deploy.log 2>&1
+cd ${SD}
+
+echo "*** Deployment started at $(date)" >> $SD/deploy.log 2>&1
 
 # REQUIRED: This is the system baseline tarball that unpacks into /home/ltfhc-deploy
 # Includes dependencies to run the application:
@@ -34,14 +38,15 @@ echo "*** Deployment started at $(date)" >> deploy.log 2>&1
 #	- Git (compiled with no HTTP support to remove external dependencies)
 if [ ! -f ./ltfhc-deploy.tar.xz ]; then
 	echo -n "Retrieving deployment package..."
-	wget http://commondatastorage.googleapis.com/ltfhc%2Fltfhc-deploy.tar.xz >> deploy.log 2>&1
+	wget http://commondatastorage.googleapis.com/ltfhc/ltfhc-deploy.tar.xz >> $SD/deploy.log 2>&1
 	echo "Done"
 fi
 
 # REQUIRED: The script clones the ltfhc-next.bundle into /home/ltfhc-deploy/ltfhc-next
-if [ ! -f ./ltfhc-next.bundle ]; then
-	echo "ltfhc-next.bundle bundle not found. Place the ltfhc-next.bundle file in the directory where this script resides."
-	exit 1
+if [ ! -f ./ltfhc-next.deploy.bundle ]; then
+	echo -n "Retrieving deployment git bundle..."
+	wget http://storage.googleapis.com/ltfhc/ltfhc-next.deploy.bundle >> $SD/deploy.log 2>&1
+	echo "Done"
 fi
 
 # OPTIONAL: The script copies any GIT bundles it finds into the /home/ltfhc-deploy/bundles directory
@@ -51,54 +56,54 @@ if [ ! -f ./*.bundle ]; then
 fi
 
 echo -n "1. Uncompressing deployment files..."
-tar Jxf ltfhc-deploy.tar.xz -C / >> deploy.log 2>&1
+tar Jxf ltfhc-deploy.tar.xz -C / >> $SD/deploy.log 2>&1
 echo "Done"
 
 echo -n "2. Installing startup scripts..."
-ln -sf /home/ltfhc-deploy/etc/init.d/couchdb /etc/init.d/couchdb  >> deploy.log 2>&1
-ln -sf /home/ltfhc-deploy/etc/default/couchdb /etc/default/couchdb  >> deploy.log 2>&1
-ln -sf /home/ltfhc-deploy/etc/logrotate.d/couchdb /etc/logrotate.d/couchdb  >> deploy.log 2>&1
-update-rc.d couchdb defaults >> deploy.log 2>&1
-rm -f /etc/nginx/sites-enabled/* >> deploy.log 2>&1
-ln -sf /home/ltfhc-deploy/etc/nginx/health /etc/nginx/sites-enabled/health >> deploy.log 2>&1
-service nginx stop >> deploy.log 2>&1
-service nginx start >> deploy.log 2>&1
+ln -sf /home/ltfhc-deploy/etc/init.d/couchdb /etc/init.d/couchdb  >> $SD/deploy.log 2>&1
+ln -sf /home/ltfhc-deploy/etc/default/couchdb /etc/default/couchdb  >> $SD/deploy.log 2>&1
+ln -sf /home/ltfhc-deploy/etc/logrotate.d/couchdb /etc/logrotate.d/couchdb  >> $SD/deploy.log 2>&1
+update-rc.d couchdb defaults >> $SD/deploy.log 2>&1
+rm -f /etc/nginx/sites-enabled/* >> $SD/deploy.log 2>&1
+ln -sf /home/ltfhc-deploy/etc/nginx/health /etc/nginx/sites-enabled/health >> $SD/deploy.log 2>&1
+service nginx stop >> $SD/deploy.log 2>&1
+service nginx start >> $SD/deploy.log 2>&1
 echo "Done"
 
 echo -n "3. Changing file permissions..."
-groupadd -f couchdb >> deploy.log 2>&1
-useradd -d /home/ltfhc-deploy -s /bin/bash -g couchdb couchdb  >> deploy.log 2>&1
-chown -R couchdb:couchdb /home/ltfhc-deploy/etc /home/ltfhc-deploy/var /home/ltfhc-deploy/share/couchdb  >> deploy.log 2>&1
+groupadd -f couchdb >> $SD/deploy.log 2>&1
+useradd -d /home/ltfhc-deploy -s /bin/bash -g couchdb couchdb  >> $SD/deploy.log 2>&1
+chown -R couchdb:couchdb /home/ltfhc-deploy/etc /home/ltfhc-deploy/var /home/ltfhc-deploy/share/couchdb  >> $SD/deploy.log 2>&1
 echo "Done"
 
 echo -n "4. Starting database..."
 # Configure CouchDB Redirect on root URL
-service couchdb start >> deploy.log 2>&1
+service couchdb start >> $SD/deploy.log 2>&1
 sleep 5
 echo "Done"
 
 echo -n "5. Installing application..."
 # Install default instance_settings
-. /home/ltfhc-deploy/env.sh >> deploy.log 2>&1
-# curl -X PUT http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr >> deploy.log 2>&1
-# curl -X POST -H "Content-Type: application/json" -d '{"_id":"_local/instance_settings","type":"prod","clinic":"'${HOSTNAME}'"}' http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr/ >> deploy.log 2>&1
+. /home/ltfhc-deploy/env.sh >> $SD/deploy.log 2>&1
+# curl -X PUT http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr >> $SD/deploy.log 2>&1
+# curl -X POST -H "Content-Type: application/json" -d '{"_id":"_local/instance_settings","type":"prod","clinic":"'${HOSTNAME}'"}' http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr/ >> $SD/deploy.log 2>&1
 
 # Install database in correct place
 cp *.bundle /home/ltfhc-deploy/bundles/
 cd /home/ltfhc-deploy/ 
-git clone bundles/ltfhc-next.bundle >> deploy.log 2>&1
+git clone bundles/ltfhc-next.deploy.bundle >> $SD/deploy.log 2>&1
 cd /home/ltfhc-deploy/ltfhc-next
-kanso push http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr >> deploy.log 2>&1
+kanso push http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr >> $SD/deploy.log 2>&1
 echo "Done."
 
 echo -n "6. Testing application..."
-curl -s http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr/_design/emr/_rewrite/|grep -i LTFHC >> deploy.log 2>&1
+curl -s http://deploy:chaiveisai9paifeich4ro0yohTiebie@localhost:5984/emr/_design/emr/_rewrite/|grep -i LTFHC >> $SD/deploy.log 2>&1
 echo "Done."
 
 echo -n "7. Bootstrapping replication..."
 cd /home/ltfhc-deploy/ltfhc-next/system/python-requests 
-python setup.py install >> deploy.log 2>&1
-/home/ltfhc-deploy/ltfhc-next/system/replication/bootstrap.py >> deploy.log 2>&1
+python setup.py install >> $SD/deploy.log 2>&1
+/home/ltfhc-deploy/ltfhc-next/system/replication/bootstrap.py >> $SD/deploy.log 2>&1
 echo "Done."
 
 echo "System ready for testing in the browser at https://www.health/"
